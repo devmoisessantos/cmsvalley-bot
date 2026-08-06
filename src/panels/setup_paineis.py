6d40b40930a4dce99b38fb1408e327ab85575283
Guxta <062moises@gmail.com>
2026-08-05 21:30:31 -0300

    src: adicionando primei passos v1.0

--- src/panels/setup_paineis.py
+++ src/panels/setup_paineis.py
@@ -326,7 +326,7 @@ async def garantir_painel_boas_vindas(
             print("❌ Guild não encontrada!")
             return
 
-        mensagem = await canal.send(view=PainelBoasVindasLayout(interaction.guild))
+        mensagem = await canal.send(view=PainelBoasVindasLayout(guild))
 
         session.add(
             PainelPostado(
